75cb65772b1189b8ce04578ed024ef816aecda82
Mczepcz <michal.czepczynski@gmail.com>
2016-05-20 12:10:33 +0200

    Stworzono nowy katalog otaz plik connection laczacy z baza danych

---
+++ src/connection.php
@@ -0,0 +1,18 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "coderslab";
+$baseName = "Twitter";
+
+$conn = new mysqli($servername,$username,$password,$baseName);
+
+if($conn->connect_error){
+    die("Polaczenie nieudane. Blad: ".$conn->connect_error);
+}else{
+   echo("Polaczenie udane</br>"); 
+   echo'<hr>';
+}
+
+
+
