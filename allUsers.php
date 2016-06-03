1f33cd7a810e34bfe9240bfe0642af30f455be94
Mczepcz <michal.czepczynski@gmail.com>
2016-06-03 17:28:06 +0200

    Bootstrap css was added

--- allUsers.php
+++ allUsers.php
@@ -3,29 +3,38 @@
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     </head>
     <body>
-        <h1>All registered users</h1>
-        <?php
-        
-        session_start();
-        require_once './src/User.php';
-        require_once './src/Tweet.php';
-        require_once './src/connection.php';
-        $sql = "SELECT * FROM User";
-        $result = $conn->query($sql);
-        if($result->num_rows > 0){
-            echo '<ul>';
-            while($row = $result->fetch_assoc()){
-                if($row['active']){
-                    echo '<li>';
-                    echo '<a href="user_page.php?id='.$row['id'].'">'.$row['full_name'].'</a>';
-                    echo '</li>';    
+        <div class="container">
+            <div class="row">
+                <ul class="nav nav-pills">
+                     <li role="presentation"><a href='index.php'>Main Page</a></li>
+                </ul>
+                    
+            </div>
+            <h1>All registered users</h1>
+            <?php
+
+            session_start();
+            require_once './src/User.php';
+            require_once './src/Tweet.php';
+            require_once './src/connection.php';
+            $sql = "SELECT * FROM User";
+            $result = $conn->query($sql);
+            if($result->num_rows > 0){
+                echo '<ul class="list-group">';
+                while($row = $result->fetch_assoc()){
+                    if($row['active']){
+                        echo '<li>';
+                        echo '<a href="user_page.php?id='.$row['id'].'">'.$row['full_name'].'</a>';
+                        echo '</li>';    
+                    }
                 }
+                echo '</ul>';
             }
-            echo '</ul>';
-        }
-        ?>
-        <a href='index.php'>Main Page</a>
+            ?>
+          
+        </div>
     </body>
 </html>
--- index.php
+++ index.php
@@ -39,32 +39,44 @@ $connectedUserId = $connectedUser->getId();
         <meta charset="UTF-8">
         <title></title>
         <script src='js/app.js'></script>
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     </head>
     <body>
-        Logged user id =<?php echo $_SESSION['loggedUserId']?><br/>
-        <a href="user_page.php?id=<?php echo $connectedUserId?>"><?php echo $connectedUser->getFullName() ?> </a><br/>
-        <b>Your Entries:</b>
-        <ul>
-            <?php
-            if($userTweets){
-                foreach ($userTweets as $tweets){
-                    $commNum = numOfComments($conn, $tweets[0]);
-                    
-                    echo '<li><a href=tweet_page.php?id='.$tweets[0].'> #'.$tweets[0].'</a><br/>'.$tweets[1].'<br/>Comments: '.$commNum.'</li>';
+        <div class="container">
+            <div class="row">
+                <ul class="nav nav-pills">
+                    <li role="presentation"><a href="logout.php">Logout</a></li>
+                    <li role="presentation"><a href='allUsers.php'>See others users</a></li>
+                    <li role="presentation"><a href="messages.php">Post box</a></li>
+                </ul>
+            </div>
+            <div class="row">
+                <h1><a href="user_page.php?id=<?php echo $connectedUserId?>"><?php echo $connectedUser->getFullName() ?> </a><small>User id :<?php echo $_SESSION['loggedUserId']?></small></h1><br/>
+            </div>
+            <div class="row">
+            <h3>Your Entries:</h3>
+            </div>
+            <ul class="list-group">
+                <?php
+                if($userTweets){
+                    foreach ($userTweets as $tweets){
+                        $commNum = numOfComments($conn, $tweets[0]);
+
+                        echo '<li class="list-group-item"><a class="list-group-item-heading" href=tweet_page.php?id='.$tweets[0].'> #'.$tweets[0].'</a><p class = "list-group-item-text">'.$tweets[1].'</p><small>Comments: <span class="label label-default">'.$commNum.'</span></small></li>';
+                    }
                 }
-            }
-            ?>
-        </ul>
-        <form method='POST'>
-            <label>Your new Entry:</label><br/>
-            <textarea id='tweet' name='tweetText' rows ='10' cols='45' maxlength='140' placeholder='Write your short message'></textarea><br/>
-            <p><span id='counter'>0</span>/140</p>
-            <input type="submit" value='Tweet'/>
-        </form>
-        <br/>
-        <a href="logout.php">Logout</a>
-        <a href='allUsers.php'>See others users</a>
-        <a href="messages.php">Post box</a>
+                ?>
+            </ul>
+            <form method='POST'>
+                <div class="form-group">    
+                    <label for="tweet">Your new Entry:</label><br/>
+                    <textarea class="form-control" id='tweet' name='tweetText' rows ='10' cols='45' maxlength='140' placeholder='Write your short message'></textarea><br/>
+                    <p><span id='counter'>0</span>/140</p>
+                    <input type="submit" class="btn btn-primary" value='Tweet'/>
+                </div>
+            </form>
+        </div>
+
 
     </body>
 </html>
